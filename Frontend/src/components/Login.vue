f992943cb1b6b9569591d3052231df90a5db6d41
adjalala kevin <kevin.adjalala@epitech.eu>
2025-10-17 12:47:56 +0100

    formulaire d'inscription et de connection

--- Frontend/src/components/Login.vue
+++ Frontend/src/components/Login.vue
@@ -2,22 +2,13 @@
     <div class="max-w-lg mx-auto p-6 bg-white rounded-lg shadow-md mt-10">
         <form action="" method="post" class="mt-6">
             <div class="mb-4">
-                <label for="name" class="block text-sm font-medium text-gray-700">Nom</label>
-                <input type="text" id="name" name="name" value="" class="mt-1 p-3 w-full border border-gray-300 outline-none rounded-md shadow-md">
-            </div>
-            <div class="mb-4">
                 <label for="email" class="block text-sm font-medium text-gray-700">email</label>
                 <input type="email" id="email" name="email" value="" class="mt-1 p-3 w-full border border-gray-300 outline-none rounded-md shadow-md">
             </div>
-            <div class="mb-4">
-                <label for="name" class="block text-sm font-medium text-gray-700">Nom</label>
-                <input type="text" id="name" name="name" value="" class="mt-1 p-3 w-full border border-gray-300 outline-none rounded-md shadow-md">
-            </div>
-            <div class="mb-4">
                 <label for="password" class="block text-sm font-medium text-gray-700">Mot de passe</label>
                 <input type="password" id="password" name="password" value="" class="mt-1 p-3 w-full border border-gray-300 outline-none rounded-md shadow-md">
             </div>
-            <button type="submit" class="py-2 px-4 bg-purple-700 hover:bg-purple-500 text-white rounded-md w-full">s'inscrire</button>
+            <button type="submit" class="py-2 px-4 bg-purple-700 hover:bg-purple-500 text-white rounded-md">Se connecter</button>
         </form>
 
     </div>
